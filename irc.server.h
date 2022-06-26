e4b5f0750a75b64bac57b640f8b9ac84e1442710
reijiken <quyduongtran0810@gmail.com>
2022-06-26 21:14:55 +0700

    first

--- irc.server.h
+++ irc.server.h
@@ -60,16 +60,31 @@ void handle_client_request(char *payload, int encryptfd, int clientfd)
     - password is the password of the client
     */
     JsonNode *payload_json = json_decode(payload);
+    char *receiver = json_find_member(payload_json, "receiver")->number;
     char *method = json_find_member(payload_json, "method")->string_;
-    char *message = json_find_member(payload_json, "message")->string_;
+    char *username = json_find_member(payload_json, "username")->string_;
+    int *password = json_find_member(payload_json, "password")->string_;
     // printf("Decrypted message: %s\n", message);
     // Tran Quy Duong: Creat the payload and send to authentication server
+    JsonNode *resquest_json = json_mkobject();
+    JsonNode *receiver_json = json_mknumber(receiver);
+    JsonNode *method_json = json_mkstring(method);
+    JsonNode *name_json = json_mkstring(username);
+    JsonNode *pass_json = json_mkstring(password);
+    json_append_member(resquest_json, "receiver", receiver_json);
+    json_append_member(resquest_json, "method", method_json);
+    json_append_member(resquest_json, "username", name_json);
+    json_append_member(request_json, "password", pass_json);
+
+    char *request_buffer = json_encode(resquest_json);
+    send(encryptfd, request_buffer, strlen(request_buffer) + 1, 0);
 }
 
 void handle_encryption_response(char *payload, int *clientfds, int encryptfd, int authfd)
 {
     /*
     This function handle the payload received from the encryption server.
+
     The method of the incoming method can either be:
     - DECRYPT => The payload is the response of a DECRYPT request, contains the login or register message to send to authentication server
     - ENCRYPT => The payload is the response of a ENCRYPT request, contains encrypted payload to send to the client
