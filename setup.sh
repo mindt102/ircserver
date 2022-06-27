ef80e7e2229a3246cd23b243f6b93f8e651a2896
Nguyen Hoang Minh <nghminh163@outlook.com>
2022-06-27 23:04:05 +0700

    fix bug

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ rm -rf test.db
 gcc ./migration.c -o migration -l sqlite3 && ./migration
 gcc irc.server.c json.c shared_func.c -o irc.server
 gcc enc.server.c json.c shared_func.c -o enc.server
-gcc gcc auth.server.c json.c shared_func.c -l sqlite3 -lssl -lcrypto -o auth.server
+gcc auth.server.c json.c shared_func.c -l sqlite3 -lssl -lcrypto -o auth.server
 gcc client.c json.c shared_func.c -o client
